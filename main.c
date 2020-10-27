0e27b8849ef4ea103be357f3ca3ed9918aad5629
anna <luribina@gmail.com>
2020-10-27 22:30:12 +0300

    fix: now it's according to the task
    сделать задание, а потом понять, что случайно выполнила не по варианту, это надо уметь так

--- main.c
+++ main.c
@@ -3,26 +3,29 @@
 #define N 4
 #define M 3
 
-int findMax(int n, int m, int matrix[M][N]) {
-    int result = matrix[0][0];
+void findMax(int n, int m, int matrix[M][N], int* result) {
+    int max=matrix[0][0];
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             int current = matrix[i][j];
-            if (current > result) result = current;
+            if (current > max) {
+                max = current;
+                result[0]=i;
+                result[1]=j;
+            }
         }
     }
-    return result;
 }
 
 int main() {
     int matrix[M][N];
-    int result;
+    int result[2]={0,0};
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-    result = findMax(N, M, matrix);
-    printf("Max element is %d", result);
+    findMax(N,M,matrix,result);
+    printf("Max element index is %d %d", result[0],result[1]);
     return 0;
 }
